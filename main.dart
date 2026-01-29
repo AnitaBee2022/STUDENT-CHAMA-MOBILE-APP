5fe09f2c5cedb9221477b53679d915a34fdb3b0d
AnitaBee2022 <113606234+AnitaBee2022@users.noreply.github.com>
2026-01-29 20:56:23 +0300

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,30 @@
+import 'package:chama/splashScreen.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
+//options: DefaultFirebaseOptions.currentPlatform,
+
+void main() async{
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Fintech',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const SplashScreen(),
+    );
+  }
+}
